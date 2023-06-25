75ff7b8c59099ed576064d4ac0184dd5324227bb
Batuhan Bolat <125748985+bolatbatu@users.noreply.github.com>
2023-06-25 16:59:33 +0300

    Update

--- serverdatayollama.py
+++ serverdatayollama.py
@@ -1,2 +1,2 @@
-from requests import put, get
-put('http://192.168.1.108:5000/todo2', data={'data': 'Batuhan Bolat'}).json()
+from requests import put, ge
+put('http://192.168.1.108:5000/todo2', data={'data': 'Batuhan Bolat'}).json()          #Bu komut sayesinde 192.168.1.108 adresin 5000 portlu adresindeki siteye todo2 adlı eklentiyle Batuhan Bolat datasını gönderdik.
